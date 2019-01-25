c908c50a03e302688c219cf67f4692e0c96ece23
Eric Liang <ekhliang@gmail.com>
2019-01-24 21:00:11 -0800

    fix bug

--- tree.py
+++ tree.py
@@ -623,9 +623,9 @@ class Tree:
 
                 # compute bytes per rule
                 if self.is_leaf(node):
-                    result["bytes_per_rule"] += 2 + 16 + 4 * len(node.children)
-                else:
                     result["bytes_per_rule"] += 2 + 16 * len(node.rules)
+                else:
+                    result["bytes_per_rule"] += 2 + 16 + 4 * len(node.children)
 
             nodes = next_layer_nodes
 
