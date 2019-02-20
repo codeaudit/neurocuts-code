fffc03c05f94b5da38b4944a23355deec48e7cab
Eric Liang <ekhliang@gmail.com>
2019-02-20 14:23:20 -0800

    fix benchmark

--- hicuts_benchmark.py
+++ hicuts_benchmark.py
@@ -4,5 +4,5 @@ from hicuts import *
 rules = load_rules_from_file("classbench/acl1_10k")
 start = time.time()
 cuts = HiCuts(rules)
-print("Total split time", time.time() - start)
 cuts.train()
+print("Total split time", time.time() - start)
